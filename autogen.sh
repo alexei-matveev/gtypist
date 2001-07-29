285e63ead3c3b9fd73ce7b9a83cc2d5a203dc2d5
michaelo <michaelo>
2001-07-29 10:08:21 +0000

    - Explicited the autoconf, automake, gettext, m4 versions to be used. - Fixed the list of generated files

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # Run this program to build the configuration
 # files for GNU Typist:
-# [*/]Makefile.in, aclocal.m4, configure
+# [*/]Makefile.in, aclocal.m4, configure,
+# config.h.in, config.sub, config.guess
+
+# WARNING: You need the following tool
+# versions to run this script:
+# autoconf-2.50, automake-1.4, m4-1.4, gettext 0.10.39
 
 # Build documentation files
 
