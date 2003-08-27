043ed5df99a105c046161adc8d65366b63983cbe
michaelo <michaelo>
2003-08-27 20:01:32 +0000

    Updated to 2.7beta3

--- version.sh
+++ version.sh
@@ -2,4 +2,4 @@
 # don't put any spaces around the version-number
 # (look at the top of configure.in to see why)
 # if you change rust rerun configure, no need to run autogen.sh
-VERSION=2.7beta2
+VERSION=2.7beta3
