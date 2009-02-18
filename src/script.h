5b766588f4bb88ba7d90e03fe45d33a3ce28fa99
Paul Goins <general@vultaire.net>
2009-02-19 00:05:34 +0900

    Removed/updated old FSF address from source files. (Reported by Wei Mingzhi)

--- src/script.h
+++ src/script.h
@@ -1,15 +1,15 @@
 /*
  * GNU Typist  - interactive typing tutor program for UNIX systems
- * 
- * Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003  
+ *
+ * Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003
  *               Simon Baldwin (simonb@sco.com)
  * Copyright (C) 2003, 2008
  *               GNU Typist Development Team <bug-gtypist@gnu.org>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +17,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef SCRIPT_H
 #define SCRIPT_H
